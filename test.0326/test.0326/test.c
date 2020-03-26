14e69db6c9858ddb55775dd9b6029447ac5d08aa
zhangdoudou-zl <1263944297@qq.com>
2020-03-26 23:38:53 +0800

    练习

---
+++ test.0326/test.0326/test.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+
+//int my_strlen(char* str)
+//
+//{
+//   if (*str != '\0')
+//		return 1 + my_strlen(str+1);                   //str++
+//	else
+//		return 0;
+//}
+//
+//#include <string.h>
+//int main()
+//
+//{
+//	char arr[] = "bit";//数组名是首元素的地址-char的地址 char*
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	int a = 1;
+//	int b = a + 1;   //a=1 b=2
+//	return 0;
+//
+//}
+
+
+
+//int Fac(int n)                     //   函数递归 计算n的阶乘
+//
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n*Fac(n - 1);
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//    int ret = Fac(n);
+//    printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+
+//
+//int Fib(int n)                  //斐波那契数列
+//{
+//
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int main()
+//
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fib(n);
+//	printf("%d\n", ret);
+//    return 0;
+//}
+
+
