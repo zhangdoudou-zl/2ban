3157bd2b3790bf5e1b2489b98a8c01b9cd99db40
zhangdoudou-zl <1263944297@qq.com>
2020-03-29 09:08:09 +0800

    扫雷

--- game/game/game.c
+++ game/game/game.c
@@ -1,23 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#inclu#define ROW 9
-#define COL 9
-
-#define ROWS ROW+2
-#define COLS COL+2
-
-#define EASY_COUNT 10
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-
+#include "game.h"
 //³õÊ¼»¯É¨À×µÄÆåÅÌ
 void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
 void DisplayBoard(char board[ROWS][COLS], int row, int col);
 void Setmine(char mine[ROWS][COLS], int row, int col);
 void Findmine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int rol);
-de "game.h"
+
 
  void InitBoard(char board[ROWS][COLS], int rows, int cols,char set)
 {
