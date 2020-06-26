e55ddd8602119f297653af6cabdb197947b7cdff
zhangdoudou-zl <1263944297@qq.com>
2020-06-26 23:06:29 +0800

    IO

---
+++ 0626/src/FileCopy.java
@@ -0,0 +1,28 @@
+/**
+ * @program: 0626
+ * @description:
+ * @author: Zhang lin
+ * @create: 2020-06-26 11:03
+ **/
+import java.io.*;
+
+public class FileCopy {
+    public static void main(String[] args) throws IOException {
+        String sourcePath = "测试目录\\中文.txt";
+        String destPath = "测试目录\\中文-复制.txt";
+
+        // 所谓的复制：读取源的内容，写入目标文件中
+        try (InputStream is = new FileInputStream(sourcePath)) {
+            try (OutputStream os = new FileOutputStream(destPath)) {
+                byte[] buffer = new byte[8192];
+                int n;
+
+                while ((n = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, n);
+                }
+
+                os.flush();
+            }
+        }
+    }
+}
